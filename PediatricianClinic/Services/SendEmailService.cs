a168dfab847d2d6136abadc1c257bea4f2ef1f6b
XLec_fst0@student.itstep.org <XLec_fst0@student.itstep.org>
2020-11-26 18:11:20 +0200

    26.11.2020

--- PediatricianClinic/Services/SendEmailService.cs
+++ PediatricianClinic/Services/SendEmailService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using MimeKit;
+using Models.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
